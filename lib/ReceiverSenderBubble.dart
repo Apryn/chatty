920bd370a3e2fa6e447ba9a8087da025183a9779
Apryn <apriyanriyan23@gmail.com>
2021-12-20 20:19:33 +0700

    Modify ReceiverSenderBubble

--- lib/ReceiverSenderBubble.dart
+++ lib/ReceiverSenderBubble.dart
@@ -3,49 +3,51 @@ import 'package:flutter/material.dart';
 class Bubble extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    
-    String? Imageurl;
-    String? text;
-    String? time;
-
-    return Container(
-      margin: EdgeInsets.only(bottom: 30),
-      child: Row(
-        children: [
-          Image.asset(
-            Imageurl!,
-            width: 40,
-            height: 40,
-          ),
-          Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(50),
-                topRight: Radius.circular(50),
-                bottomRight: Radius.circular(50),
-              ),
+    Widget receiverbubble({
+      String? Imageurl,
+      String? text,
+      String? time,
+    }) {
+      return Container(
+        margin: EdgeInsets.only(bottom: 30),
+        child: Row(
+          children: [
+            Image.asset(
+              Imageurl!,
+              width: 40,
+              height: 40,
             ),
-            child: Column(
-              children: [
-                Text(
-                  text!,
-                  style: TextStyle(
-                      fontSize: 16,
-                      fontFamily: 'Poppins',
-                      fontWeight: FontWeight.w400),
+            Container(
+              decoration: BoxDecoration(
+                color: Color(0xffEBEFF3),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(50),
+                  topRight: Radius.circular(50),
+                  bottomRight: Radius.circular(50),
                 ),
-                Text(
-                  time!,
-                  style: TextStyle(
-                      fontSize: 14,
-                      fontFamily: 'Poppins',
-                      fontWeight: FontWeight.w300),
-                )
-              ],
-            ),
-          )
-        ],
-      ),
-    );
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    text!,
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w400),
+                  ),
+                  Text(
+                    time!,
+                    style: TextStyle(
+                        fontSize: 14,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w300),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      );
+    }
   }
 }
